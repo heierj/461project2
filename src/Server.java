f6ceb8235dfd7ffb738a43fb7dad6a6ba3e12bc2
Jordan Heier <heierj@uw.edu>
2014-10-21 18:36:25 +0100

    Some very basic setup

---
+++ src/Server.java
@@ -0,0 +1,10 @@
+/** 
+ * This class will act as the entry point for clients. It will
+ * receive the first packet and then spin off a thread to handle 
+ * the session for that client.
+ * 
+ * @author Jordan Heier, Cameron Hardin, Will McNamara
+ */
+public class Server {
+
+}
---
+++ src/Session.java
@@ -0,0 +1,11 @@
+/**
+ * This class will handle the management of a particular
+ * client's session.
+ * 
+ * @author Jordan Heier, Cameron Hardin, Will McNamara
+ */
+public class Session {
+	private int studentNum;
+	private int secretA, secretB, secretC, secretD;
+	private int portB, portC;
+}
